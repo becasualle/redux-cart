2d959724facdf301c847e6d17ffb1dc7cdfd5a0d
Mark <mark.nelubin@gmail.com>
2021-09-14 18:49:13 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
 import React from "react";
-// components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
 import cartItems from "./cart-items";
-// redux stuff
-
 import { createStore } from 'redux';
+import reducer from "./reducer";
 
 const initialStore = {
-  count: 0
-}
-
-function reducer(state, action) {
-  console.log({ state, action });
-  if (action.type === 'DECREASE') {
-
-    return { count: state.count - 1 }
-  }
-  if (action.type === 'INCREASE') {
-
-    return { count: state.count + 1 }
-  }
-  if (action.type === 'RESET') {
-
-    return { count: 0 }
-  }
-  return state;
+  cart: cartItems,
+  total: 0,
+  amount: 0
 }
 
 const store = createStore(reducer, initialStore);
-store.dispatch({ type: 'DECREASE' })
-store.dispatch({ type: 'INCREASE' })
-store.dispatch({ type: 'INCREASE' })
-store.dispatch({ type: 'RESET' })
-
-
-console.log(store.getState())
 
 function App() {
-  // cart setup
 
   return (
     <main>
-      <Navbar cart={store.getState()} />
+      <Navbar />
       <CartContainer cart={cartItems} />
     </main>
   );
