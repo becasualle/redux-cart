ba94868ad901fe4ccd1e168f5a81d5b4526edc19
Mark <mark.nelubin@gmail.com>
2021-09-16 19:32:29 +0300

    added Remove, Increase and Decrease

--- src/reducer.js
+++ src/reducer.js
@@ -7,15 +7,34 @@ export default function reducer(state, action) {
     }
 
     if (action.type === DECREASE) {
-        console.log('you decreased amount')
+        let tempCart = [];
+        if (action.payload.amount === 1) {
+            tempCart = state.cart.filter(item => item.id !== action.payload.id)
+        } else {
+            tempCart = state.cart.map(item => {
+                if (item.id === action.payload.id) {
+                    item = { ...item, amount: item.amount - 1 }
+                }
+                return item
+            })
+        }
+
+        return { ...state, cart: tempCart };
     }
 
     if (action.type === INCREASE) {
-        console.log('you increased amount')
+        let tempCart = state.cart.map(item => {
+            if (item.id === action.payload.id) {
+                item = { ...item, amount: item.amount + 1 }
+            }
+            return item
+        })
+
+        return { ...state, cart: tempCart }
     }
 
     if (action.type === REMOVE) {
-        console.log('you removed amount')
+        return { ...state, cart: state.cart.filter(item => item.id !== action.payload.id) }
     }
 
     return state;
